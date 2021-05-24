576bc03d795b9facbd7c1ba8fa74136085f16f1a
Katerina Barros <katerina.barros@alumnos.frm.utn.edu.ar>
2021-05-24 11:13:15 -0300

    a

---
+++ GrupoH-TT/Proyecto/Token.py
@@ -0,0 +1,28 @@
+import pygame
+
+import Tablero
+
+class Token:
+
+    def __init__(self, Nombre, Icono, Movimiento, Efecto, PuntajeMax, Terreno, Rect):
+
+        self.Nombre = Nombre
+        self.Icono = Icono
+        self.Movimiento = Movimiento
+        self.Efecto = Efecto
+        self.PuntajeMax = PuntajeMax
+        self.TerrenoFavorable = Terreno
+        self.r = pygame.Rect
+        self.Posicion = None
+        self.Seleccionado = False
+
+    def pintarCasillas(self, casilla, screen):
+        print("Flag Mover")
+        Esperar = True
+        while Esperar:
+            pos_Tok_X , pos_Tok_Y = casilla.pos
+            for i in range (self.Movimiento):
+                pygame.draw.rect(screen, (0,127,0), Tablero.tablero_Hash[(pos_Tok_X + i,pos_Tok_Y + i)].r)
+
+    def efecto():
+        pass
